9432166f1ebf474aaf9421d2812f9d666d22fe3f
Jimmy Metz <jmetz@ultramain.com>
2022-12-12 21:37:16 -0700

    Initial commit

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import PageLoading from '../PageLoading';
--- src/components/UserPostsPage/UserPostsPage.jsx
+++ src/components/UserPostsPage/UserPostsPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import { Link, useParams } from 'react-router-dom';
 import PageLoading from '../PageLoading';
